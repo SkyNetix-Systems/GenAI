03f2ef9d426bff3f452c4d14fd2eeaf8244c22b3
Akhilesh Shende <akhilesh@skynetixsystems.com>
2025-02-09 10:03:06 +0530

    fixed error

--- Gemini/ai-generator/app/page.tsx
+++ Gemini/ai-generator/app/page.tsx
@@ -1,15 +1,31 @@
 "use client";
 import { runAI } from "./actions/ai";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 export default function Page() {
   const [response, setResponse] = useState("");
   const [loading, setLoading] = useState(false);
+  const [query, setQuery] = useState("");
 
-  const handleClick = async () => {
+  useEffect(() => {
+    setQuery(""); // Ensure state is updated after the component mounts
+  }, []);
+
+  const handleClick = async (e: any) => {
+    e.preventDefault();
     setLoading(true);
     try {
-      const data = await runAI("How are you?");
+      const data = await runAI(query);
       setResponse(data);
     } catch (error) {
       console.error(error);
@@ -19,12 +35,23 @@ export default function Page() {
   };
 
   return (
-    <>
-      <button className="custom-button" onClick={handleClick}>
-        What can I do for you?
-      </button>
-      <br />
-      <div>{loading ? "Loading..." : response}</div>
-    </>
+    <div className="m-5">
+      <form onSubmit={handleClick}>
+        <input
+          className="mb-5 bordered-input"
+          placeholder="Ask anything!"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <br />
+        <Button>Generate with AI</Button>
+      </form>
+      <Card className="mt-5">
+        <CardHeader>AI Response</CardHeader>
+        <CardContent>
+          <div>{loading ? "Loading..." : response}</div>
+        </CardContent>
+      </Card>
+    </div>
   );
 }
