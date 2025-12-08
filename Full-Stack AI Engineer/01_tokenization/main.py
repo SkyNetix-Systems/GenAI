5f84d2f809d847ad4764963baab16fee5994b3e9
Akhilesh Shende <akhilesh@skynetixsystems.com>
2025-12-08 22:45:49 +0530

    added .env

--- Full-Stack AI Engineer/01_tokenization/main.py
+++ Full-Stack AI Engineer/01_tokenization/main.py
@@ -5,8 +5,8 @@ enc = tiktoken.encoding_for_model("gpt-4o")
 text = "Hey There! My name is Akhilesh"
 tokens = enc.encode(text)
 
-# Tokens [25216, 3274, 0, 3673, 1308, 382, 398, 3403, 1776, 170676]
-print("Tokens", tokens)
+# Tokens: [25216, 3274, 0, 3673, 1308, 382, 13232, 71, 2892, 71]
+print("Tokens: ", tokens)
 
-decoded = enc.decode([25216, 3274, 0, 3673, 1308, 382, 398, 3403, 1776, 170676])
-print("Decoded", decoded)
+decoded = enc.decode([25216, 3274, 0, 3673, 1308, 382, 13232, 71, 2892, 71])
+print("Decoded: ", decoded)
